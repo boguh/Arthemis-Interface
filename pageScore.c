b887e5a491d5cf2adc3c9130191b66617439bfc2
boguh <106980545+boguh@users.noreply.github.com>
2023-01-24 14:29:58 +0100

    pageScore patch

--- pageScore.c
+++ pageScore.c
@@ -19,6 +19,12 @@ static uint16_t score_loc = 0;
 //Variables static permettant de retenir le score des visiteurs
 static uint16_t score_visit = 0;
 
+static volatile uint8_t seconds = 60;
+static volatile uint16_t t = 0;
+static uint16_t chrono_color = 0xF000;
+static bool_e chrono_state = TRUE;
+static char chrono_String[5];
+
 //------------------------------------------------------------------------------------------------------------------
 
 /*
@@ -29,37 +35,41 @@ void pageScore_DrawPage(void) {
 	ihm_CleanPage();
 	ILI9341_Rotate(ILI9341_Orientation_Landscape_2);
 
-	ihm_DrawFilledRoundedRectangle(60,10,150,200,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(170,10,260,200,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(0,210,140,240,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(180,210,320,240,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(10,10,100,60,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(10,80,100,130,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(10,150,100,200,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(220,10,310,60,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(220,80,310,130,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(220,150,310,200,20,ILI9341_COLOR_NOIR_ihm);
-	ihm_DrawFilledRoundedRectangle(120,0,200,50,20,ILI9341_COLOR_WHITE);
+	ihm_DrawFilledRoundedRectangle(10,10,100,60,20,0x8610);
+	ILI9341_Puts(30,25, "+1", &Font_11x18, ILI9341_COLOR_BLACK, 0x8610);
 
-	ILI9341_Puts(130,20, "", &Font_11x18, ILI9341_COLOR_RED, ILI9341_COLOR_WHITE);
-	ILI9341_Puts(10,10, "+1", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
-	ILI9341_Puts(10,80, "+2", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
-	ILI9341_Puts(10,150, "+3", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+	ihm_DrawFilledRoundedRectangle(10,80,100,130,20,0x3DE7);
+	ILI9341_Puts(30,95, "+2", &Font_11x18, ILI9341_COLOR_BLACK, 0x3DE7);
+
+	ihm_DrawFilledRoundedRectangle(10,150,100,200,20,0x2384);
+	ILI9341_Puts(30,165, "+3", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, 0x2384);
+
+	ihm_DrawFilledRoundedRectangle(60,10,150,200,20,ILI9341_COLOR_NOIR_ihm);
 	char scoreS1[3];
 	sprintf(scoreS1, "%d", score_loc);
-	ILI9341_Puts(105,70, scoreS1, &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+	ILI9341_Puts(100,100, scoreS1, &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+	ILI9341_Puts(90,120, "LOC", &Font_11x18, 0x8610, ILI9341_COLOR_NOIR_ihm);
 
-	ILI9341_Puts(260,10, "+1", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
-	ILI9341_Puts(260,80, "+2", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
-	ILI9341_Puts(260,150, "+3", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+	ihm_DrawFilledRoundedRectangle(220,10,310,60,20,0x759B);
+	ILI9341_Puts(270,25, "+1", &Font_11x18, ILI9341_COLOR_BLACK, 0x759B);
+
+	ihm_DrawFilledRoundedRectangle(220,80,310,130,20,0x1C3A);
+	ILI9341_Puts(270,95, "+2", &Font_11x18, ILI9341_COLOR_BLACK, 0x1C3A);
+
+	ihm_DrawFilledRoundedRectangle(220,150,310,200,20,0x01AB);
+	ILI9341_Puts(270,165, "+3", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, 0x01AB);
+
+	ihm_DrawFilledRoundedRectangle(170,10,260,200,20,ILI9341_COLOR_NOIR_ihm);
 	char scoreS2[3];
 	sprintf(scoreS2, "%d", score_visit);
-	ILI9341_Puts(215,70, scoreS2, &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+	ILI9341_Puts(220,100, scoreS2, &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+	ILI9341_Puts(190,120, "VISIT", &Font_11x18, 0x759B, ILI9341_COLOR_NOIR_ihm);
 
-	ILI9341_Puts(30,220, "Chrono", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_GREEN);
-	ILI9341_Puts(210,220, "Fautes", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
-	ILI9341_Puts(70,170, "Locaux", &Font_11x18, ILI9341_COLOR_GREEN, ILI9341_COLOR_NOIR_ihm);
-	ILI9341_Puts(180,170, "Visiteur", &Font_11x18, ILI9341_COLOR_BLUE, ILI9341_COLOR_NOIR_ihm);
+	ihm_DrawFilledRoundedRectangle(90,210,230,240,20,ILI9341_COLOR_NOIR_ihm);
+	ILI9341_Puts(130,220, "FAUTES", &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+
+	ILI9341_DrawFilledRectangle(120,0,200,50,0x3165);
+	ILI9341_Puts(135,20, " ", &Font_11x18, 0xF000, 0x3165);
 
 }
 
@@ -77,7 +87,7 @@ void pageScore_Actions(int16_t x, int16_t y) {
 		score_loc++;
 		char score[3];
 		sprintf(score, "%d", score_loc);
-		ILI9341_Puts(105,70, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+		ILI9341_Puts(100,100, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
 	}
 
 	//Incr�mente le score1 de 3
@@ -85,7 +95,7 @@ void pageScore_Actions(int16_t x, int16_t y) {
 		score_loc = score_loc+3;
 		char score[3];
 		sprintf(score, "%d", score_loc);
-		ILI9341_Puts(105,70, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+		ILI9341_Puts(100,100, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
 	}
 
 	//Incr�mente le score1 de 2
@@ -93,7 +103,7 @@ void pageScore_Actions(int16_t x, int16_t y) {
 		score_loc = score_loc+2;
 		char score[3];
 		sprintf(score, "%d", score_loc);
-		ILI9341_Puts(105,70, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+		ILI9341_Puts(100,100, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
 	}
 
 	//Incr�mente le score2 de 1
@@ -101,7 +111,7 @@ void pageScore_Actions(int16_t x, int16_t y) {
 		score_visit++;
 		char score[3];
 		sprintf(score, "%d", score_visit);
-		ILI9341_Puts(215,70, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+		ILI9341_Puts(220,100, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
 	}
 
 	//Incr�mente le score2 de 3
@@ -109,7 +119,7 @@ void pageScore_Actions(int16_t x, int16_t y) {
 		score_visit = score_visit+3;
 		char score[3];
 		sprintf(score, "%d", score_visit);
-		ILI9341_Puts(215,70, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+		ILI9341_Puts(220,100, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
 	}
 
 	//Incr�mente le score2 de 2
@@ -117,7 +127,7 @@ void pageScore_Actions(int16_t x, int16_t y) {
 		score_visit = score_visit+2;
 		char score[3];
 		sprintf(score, "%d", score_visit);
-		ILI9341_Puts(215,70, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+		ILI9341_Puts(220,100, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
 	}
 
 	// Enleve 1 point � 1 pour corriger si jamais erreur (en clic sur le score1)
@@ -126,7 +136,7 @@ void pageScore_Actions(int16_t x, int16_t y) {
 			score_loc--;
 			char score[3];
 			sprintf(score, "%d", score_loc);
-			ILI9341_Puts(105,70, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+			ILI9341_Puts(100,100, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
 		}
 	}
 
@@ -136,7 +146,7 @@ void pageScore_Actions(int16_t x, int16_t y) {
 			score_visit--;
 			char score[3];
 			sprintf(score, "%d", score_visit);
-			ILI9341_Puts(215,70, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
+			ILI9341_Puts(220,100, score , &Font_11x18, ILI9341_COLOR_GRIS2_ihm, ILI9341_COLOR_NOIR_ihm);
 		}
 	}
 
@@ -146,9 +156,17 @@ void pageScore_Actions(int16_t x, int16_t y) {
 		ihm_AffichePageCourante();
 	}
 
-	// Retour en arriere
-	if ((x>=0 && x<=140)&&(y>=210 && y<=240)) {
-		Systick_add_callback_function(&chrono);
+	//TODO : quand on clique sur le chrono "00:00" lance celui-ci, si déjà lancer cela le met en pause
+	if ((x>=120 && x<=200)&&(y>=0 && y<=50)) {
+		if (chrono_state) {
+			chrono_color = 0xF000;
+			Systick_add_callback_function(&chrono);
+			chrono_state = FALSE;
+		} else {
+			chrono_color = 0xFDE5;
+			Systick_remove_callback_function(&chrono);
+			chrono_state = TRUE;
+		}
 	}
 }
 
@@ -179,16 +197,14 @@ uint16_t getScoreVisit(void) {
  */
 
 void chrono(void) {
-	static uint8_t seconds = 60;
-	static uint16_t t = 0;
+	//TODO : fix it
 	t++;
 	if (t==1000) {
 		if (seconds) {
 			seconds--;
 		}
-		char chrono_String[3];
 		sprintf(chrono_String, "%d", seconds);
-		ILI9341_Puts(130, 20, chrono_String, &Font_11x18, ILI9341_COLOR_RED, ILI9341_COLOR_NOIR_ihm);
+		ILI9341_Puts(130, 20, chrono_String, &Font_11x18, chrono_color, 0x3165);
 		t = 0;
 	}
 }
